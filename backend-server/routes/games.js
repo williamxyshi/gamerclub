3ff2fdb17e53a39dff4102c0f250d437cc2cb4d3
williamxyshi <williamxyshi@gmail.com>
2020-07-05 17:37:50 -0400

    backend search component complete

--- backend-server/routes/games.js
+++ backend-server/routes/games.js
@@ -13,6 +13,9 @@ const User = require("../models/user_model");
 
 const igdb = require('igdb-api-node').default;
 
+const axios = require('axios');
+const { response } = require("express");
+
 const client = igdb('18cd0fe5d2781aaa3c13611e304dc2cd');
 
 router.get("/getgame", async (req, res) => {
@@ -22,22 +25,57 @@ router.get("/getgame", async (req, res) => {
 
 
         
-     
+        const RAWGresponse = await axios.get('https://api.rawg.io/api/games?search=' + gameName)
+
+
+        var responseArr =[]
+
+
+        var maxLength = 4
+        if(RAWGresponse.data.results.length < 4){
+            maxLength = RAWGresponse.data.results.length
+        } 
+
+
+        for(i = 0; i< maxLength; i++){
+            var responsegamename = RAWGresponse.data.results[i].name
+
+            console.log(responsegamename)
+
+
+            var url = ""
+
+            try{
+                await wiki().page(responsegamename).then( page => page.mainImage() ).then(res=>{
+                    url = res;
+                    console.log(res)
+                });
+            } catch(e){
+                console.log("error retriving image" + e)
+            }
+      
+
+
+            responseArr.push(
+                [
+                    responsegamename, url
+                ]
+            );
+    
+        }
+
+
+        console.log(responseArr)
 
 
 
-
-        var url = ""
+       
 
  
         
-        await wiki().page(gameName).then( page => page.mainImage() ).then(res=>{
-            url = res;
-            console.log(res)
-        });
+  
 
-
-        res.json(url)
+        res.json(responseArr)
 
     // } catch (e) {
     //   res.send({ message: "Error in Fetching user" + e});
