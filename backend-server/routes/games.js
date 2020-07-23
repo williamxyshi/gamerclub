02daedfbdd8a647def1c610f218ef0b1a87fb4f8
williamxyshi <williamxyshi@gmail.com>
2020-07-23 10:03:15 -0400

    adding necessary backend api endpoints

--- backend-server/routes/games.js
+++ backend-server/routes/games.js
@@ -24,6 +24,53 @@ const { response } = require("express");
 const client = igdb('18cd0fe5d2781aaa3c13611e304dc2cd');
 
 /**
+ * add post
+ */
+router.post("/addpost", auth, async(req,res) => {
+    try {
+
+        const user = await User.findById(req.user.id);
+
+        if(!user){
+            return res.status(400).json({
+                msg: "Invalid User"
+            });
+        }
+
+        const {
+            id
+        } = req.body;
+
+        let club = await Club.findOne({
+            id
+        });
+
+        if (!club) {
+            return res.status(400).json({
+                msg: "Invalid Club ID"
+            });
+        }
+        
+        if(!isInClub){
+            return res.status(400).json({
+                msg: "User Not In Club"
+            });
+        }
+
+
+
+    
+        
+        res.json(user)
+
+    } catch (e) {
+
+      res.send({ message: "Could Not Leave Club"});
+
+    }
+})
+
+/**
  * get game endpoint api 
  * 
  * returns 3 games and their images
@@ -61,6 +108,97 @@ router.get("/getgame", async (req, res) => {
   });
 
 /**
+ * leave club
+ */
+router.post("/leaveclub", auth, async(req,res) => {
+    try {
+
+        const user = await User.findById(req.user.id);
+
+        if(!user){
+            return res.status(400).json({
+                msg: "Invalid User"
+            });
+        }
+
+        const {
+            id
+        } = req.body;
+
+        let club = await Club.findOne({
+            id
+        });
+
+        if (!club) {
+            return res.status(400).json({
+                msg: "Invalid Club ID"
+            });
+        }
+
+          /**
+         * Check user is in club
+         */
+
+        var isInClub = false;
+        for(i = 0; i< club.members.length; i++){
+
+            let currentM = club.members[i];
+
+            if (currentM.email == user.email){
+                if(currentM.isadmin == true){
+
+                    console.log(currentM.isadmin)
+                    return res.status(400).json({
+                        msg: "Cannot Leave Your Own Club"
+                    });
+
+                }
+                isInClub = true;
+                break;
+            }
+        }
+        
+        if(!isInClub){
+            return res.status(400).json({
+                msg: "User Not In Club"
+            });
+        }
+
+
+        /**
+         * remove user from club
+         */
+        function checkUser(value){
+            return value.email != user.email;
+        }
+
+        club.members = club.members.filter(checkUser)
+        club.reachedDeadline = club.reachedDeadline.filter(checkUser)
+        club.save()
+
+
+        /**
+         * remove club from user
+         */
+        function checkClub(value){
+            return value.clubid != id
+        }
+
+        user.joinedclubs = user.joinedclubs.filter(checkClub)
+        user.save()
+
+    
+        
+        res.json(user)
+
+    } catch (e) {
+
+      res.send({ message: "Could Not Leave Club"});
+
+    }
+})
+
+/**
  * join club with members
  */
 router.post("/joinclub", auth, async(req,res) => {
@@ -78,7 +216,12 @@ router.post("/joinclub", auth, async(req,res) => {
 
         const user = await User.findById(req.user.id);
 
-        console.log(user)
+        if(!user){
+            return res.status(400).json({
+                msg: "Invalid User"
+            });
+        }
+
 
         const {
             id
@@ -94,11 +237,20 @@ router.post("/joinclub", auth, async(req,res) => {
             });
         }
 
-          /**
+        /**
          * Check if already part of club
          */
 
-        console.log("club found: " + club);
+        for(i = 0; i< club.members.length; i++){
+
+            let currentM = club.members[i];
+            if (currentM.email == user.email){
+                return res.status(400).json({
+                    msg: "User Already In Club"
+                });
+            }
+
+        }
 
         let member = {
             email: user.email,
@@ -107,19 +259,52 @@ router.post("/joinclub", auth, async(req,res) => {
 
         }
 
+        let joinedclub = {
+            clubid: id
+        }
         //add to clubs member list
+        Club.findOneAndUpdate({id: id}, {$push: {members: member}},{new: true}, (err, result) => {
+            console.log(err, result);
+          })
 
         //add club to users club list
+        User.findOneAndUpdate({email: user.email}, {$push: {joinedclubs: joinedclub}},{new: true}, (err, result) => {
+            console.log(err, result);
+          })
+
+
 
         //make post about new user joining club
+        let clubid = id;
 
+        const posts = await Post.find({
+            clubid: id
+        })
         
+        let postid = posts.length
+
+        let posttitle = user.username + " has joined the club!";
+        let postdescription = "Please give them a warm welcome"
+        let posttype = "announcement";
+        let dateposted = date;
+
+        let posteremail = "a@gmail.com"
+
+        let post = new Post({
+            postid,
+            clubid,
+            posttitle,
+            postdescription,
+            posteremail,
+            
+            posttype,
+            dateposted
+        })
+        console.log(post);
+        post.save();
 
 
-
-
-
-
+    
         
         res.json(club)
 
@@ -179,7 +364,6 @@ router.post("/createclub", auth, async (req, res) => {
         let year = date_ob.getFullYear();
         let date = year + "-" + month + "-" + day
 
-        console.log(date);
         
         let datecreated = date;
         let startedplaying = date;
