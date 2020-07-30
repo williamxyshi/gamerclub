c8b577967d0ac9ce971e980ba61b72048fbee223
williamxyshi <williamxyshi@gmail.com>
2020-07-30 18:59:39 -0400

    added collaspable comments menu

--- frontend/src/components/club/page/PostsComponent.js
+++ frontend/src/components/club/page/PostsComponent.js
@@ -3,6 +3,8 @@ import M from "materialize-css";
 
 import axios from "axios";
 
+import Collapsible from 'react-collapsible';
+
 
 const test = []
 
@@ -32,11 +34,26 @@ class PostsComponent extends Component {
     return (
        <ul>
             {postsComments.map((item, index) => (
-                <div style={{}}>
+                <div style={{borderColor: "#000", borderStyle:"solid", borderWidth: "1px", marginTop: 10, marginBottom: 5}}>
 
                     {console.log(postsComments[index])}
 
-                    {postsComments[index].post.posttitle}
+                    <div style={{fontSize: 18, fontFamily:"monospace"}}>
+                        <b>{postsComments[index].post.posttitle}</b>
+                    </div>
+                
+                    <div style={{fontSize: 14, fontFamily: "monospace"}}>
+
+                        {postsComments[index].post.postdescription}
+
+
+                    </div>
+
+                    <Collapsible trigger="Start here">
+                        <p>This is the collapsible content. It can be any element or React component you like.</p>
+                        <p>It can even be another Collapsible component. Check out the next section!</p>
+                    </Collapsible>
+
 
 
 
